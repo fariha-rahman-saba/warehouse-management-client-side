2fb9857ba510c4beb7b3a7aa01739dddbf73e203
saba rahman <sabarahman.official@gmail.com>
2022-04-29 20:48:00 +0600

    basic component setup

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>Book Mania</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav.Link className='text-dark' as={Link} to='/'>Home</Nav.Link>
+                        <Nav.Link className='text-dark' as={Link} to='/blogs'>Blogs</Nav.Link>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    );
+};
+
+export default Header;
