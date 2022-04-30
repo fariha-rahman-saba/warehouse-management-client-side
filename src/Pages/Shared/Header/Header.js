63c1db3eb2842936fe930fcc862a559f366ced0a
saba rahman <sabarahman.official@gmail.com>
2022-04-30 09:57:05 +0600

    signout working

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,19 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 
 const Header = () => {
+
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    };
+
     return (
         <div>
             <Navbar bg="light" expand="lg">
@@ -14,9 +24,18 @@ const Header = () => {
                         <Nav.Link className='text-dark' as={Link} to='/'>Home</Nav.Link>
                         <Nav.Link className='text-dark' as={Link} to='/blogs'>Blogs</Nav.Link>
                         <Nav.Link className='text-dark' as={Link} to='/add-items'>Add Items</Nav.Link>
-                        <Nav.Link className='text-dark' as={Link} to='/login'>
-                            Login
-                        </Nav.Link>
+
+                        {/* Handle signout and login in the navbar */}
+
+                        {
+                            user ?
+                                <button className='btn btn-link text-dark text-decoration-none' onClick={handleSignOut}>Sign Out</button>
+                                :
+                                <Nav.Link className='text-dark' as={Link} to='/login'>
+                                    Login
+                                </Nav.Link>
+
+                        }
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
