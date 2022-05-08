90517862c49514c63367b74e66bfaecf3af19838
saba rahman <sabarahman.official@gmail.com>
2022-05-08 18:49:17 +0600

    not found page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const NotFound = () => {
+    const img = `https://images.unsplash.com/photo-1578328819058-b69f3a3b0f6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80`;
     return (
         <div>
-            <h1>Page not found</h1>
+            <h1 className='m-5'>Sorry! Page not found</h1>
+            <img className='w-100' src={img} alt="" height="500px" />
         </div>
     );
 };
 
-export default NotFound;
+export default NotFound;;
